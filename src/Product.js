ea452c358f389be263cd6acfffb8e1e61aa00ebc
jay <jgrinols@cs.uw.edu>
2021-09-20 09:06:36 -0700

    reduced product name word size

--- src/Product.js
+++ src/Product.js
@@ -9,7 +9,7 @@ function Product(props) {
         <NavLink to={"/" + props.name}>
         <div style={{cursor: "pointer"}}>
             <Paper elevation={0} style={{display: 'inline-block'}}>
-                <img src={props.src} style={{width: "100%", minWidth: "100px", padding:"0px", margin: "0px"}}></img>
+                <img src={props.src} style={{width: "100%", minWidth: "100%", padding:"0px", margin: "0px"}}></img>
                 <p style={{paddingLeft: "0%", color: "#c7a2c4", fontSize: "160%", textAlign: "left", fontFamily: 'Nanum Pen Script', paddingTop: "5px", padding:"0px", margin: "0px"}}><h4 style={{padding:"0px", margin: "0px"}}>{props.name}</h4><h4 style={{fontFamily: "klee one", fontSize: "60%", padding:"0px", margin: "0px", textAlign: "right"}}>{props.price}</h4></p>
             </Paper>
         </div>
