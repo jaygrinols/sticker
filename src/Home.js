9b79f6d6b02b41ee23942893fb64933309e12203
jay <jgrinols@cs.uw.edu>
2021-09-24 00:13:30 -0700

    tidied up homepage carousel

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import Box from '@material-ui/core/Box'
 import Carousel from 'react-material-ui-carousel' //https://www.npmjs.com/package/react-material-ui-carousel
+import { NavLink } from 'react-router-dom';
 
 function Home() { //IMPLEMENT TITLE BAR BELOW IMAGE DEMO material ui // SLIDE SHOW FOR FEATURED INSTEAD OF IMAGELIST
     return (
       <div id='home'>
         <br/>
         <Box sx={{ width: "100%", marginLeft: 'auto', marginRight: 'auto', position:'relative'}} >
-          <Carousel>
-            <img alt="" style={{width:"75%"}} src="BannerHome1.jpeg"/>
-            <img alt="" style={{width:"75%"}} src="BannerHome2.png"/>
+          <Carousel animation="fade" autoplay={true} stopAutoPlayOnHover={false} interval={7000} navButtonsAlwaysInvisible={true}>
+            <NavLink to="/shop"><img alt="" style={{width:"90%"}} src="BannerHome1.jpeg"/></NavLink>
+            <NavLink to="/shop"><img alt="" style={{width:"90%"}} src="BannerHome2.png"/></NavLink>
           </Carousel>
         </Box>
       </div>
